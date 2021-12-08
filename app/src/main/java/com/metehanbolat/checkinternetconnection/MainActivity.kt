7c44ffd1ed4194a9ff72f07eb7639211a7a32376
metehanie <metehan.bolat.ie@gmail.com>
2021-12-08 23:46:11 +0300

    NetworkConnection observed.

--- app/src/main/java/com/metehanbolat/checkinternetconnection/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/checkinternetconnection/MainActivity.kt
@@ -2,6 +2,7 @@ package com.metehanbolat.checkinternetconnection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.metehanbolat.checkinternetconnection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val networkConnection = NetworkConnection(applicationContext)
+        networkConnection.observe(this){ isConnected ->
+            if (isConnected){
+                binding.layoutDisconnected.visibility = View.GONE
+                binding.layoutConnected.visibility = View.VISIBLE
+            }else{
+                binding.layoutDisconnected.visibility = View.VISIBLE
+                binding.layoutConnected.visibility = View.GONE
+            }
+        }
     }
 }
