1d9a838eacd67090dde70ab70bbe79dfc0b5e2a3
metehanie <metehan.bolat.ie@gmail.com>
2021-12-08 23:30:43 +0300

    UI completed.

--- app/src/main/java/com/metehanbolat/checkinternetconnection/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/checkinternetconnection/MainActivity.kt
@@ -2,10 +2,16 @@ package com.metehanbolat.checkinternetconnection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.metehanbolat.checkinternetconnection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
